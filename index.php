65df62834771cb5e2e86818051e90c798c81d93a
Bruno Tavares <bruno.tavares@sencha.com>
2013-06-24 13:59:07 -0700

    Deploy to Heroku

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 $isDevelopment = $_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '127.0.0.1';
 
 if ($isDevelopment) {
-	 header( 'Location: ./index-dev.html');
+	 header( 'Location: ./index-app.html');
 }
 else {
-	header( 'Location: ./build/Radio/production/index.html');
+	header( 'Location: ./build/Radio/production/index-app.html');
 }
 ?>
